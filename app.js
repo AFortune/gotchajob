79437489f734e939acf925e851cdb3e71b7fdb6c
Aaron Fortune <aaron.fortune@icloud.com>
2015-12-24 09:18:14 -0500

    Added styling to the main app page.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var request = require('request');
+var async = require('async');
 var jobIds = [];
 var jobData = [];
 request('https://hacker-news.firebaseio.com/v0/jobstories.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
       jobIds = JSON.parse(body);
       jobIds.forEach(function(value) {
-          request('https://hacker-news.firebaseio.com/v0/item/'+ value +'.json?print=pretty', function (error, response, body) {
+          request('https://hacker-news.firebaseio.com/v0/item/'+ value +'.json', function (error, response, body) {
               if (!error && response.statusCode == 200) {
                   jobData.push(JSON.parse(body));
               }
           });
       });
       console.log(jobData);
+      jobData.forEach(function(value) {
+          var p = document.createElement('p');
+          p.textContent = value.toString();
+          document.body.appendChild(p);
+      });
   }
 });
