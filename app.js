f9b7612a31a879b6c47860465d1297d804385b34
Aaron Fortune <aaron.fortune@icloud.com>
2015-12-29 05:43:26 -0500

    have hn data now loading into the index page.

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
 var request = require('request');
-var async = require('async');
-var jobIds = [];
-var jobData = [];
-request('https://hacker-news.firebaseio.com/v0/jobstories.json', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-      jobIds = JSON.parse(body);
-      jobIds.forEach(function(value) {
-          request('https://hacker-news.firebaseio.com/v0/item/'+ value +'.json', function (error, response, body) {
-              if (!error && response.statusCode == 200) {
-                  jobData.push(JSON.parse(body));
-              }
-          });
-      });
-      console.log(jobData);
-      jobData.forEach(function(value) {
-          var p = document.createElement('p');
-          p.textContent = value.toString();
-          document.body.appendChild(p);
-      });
-  }
-});
+var Q = require('q');
+
+function requestPromise(url) {
+    var deferred = Q.defer();
+    request(url, function (error, response, body) {
+        (error) ? deferred.reject(error) : deferred.resolve(JSON.parse(body));
+    })
+    return deferred.promise;
+}
+
+function getJobData(value) {
+    var array = Q.all(value.map(function(el){// this fixes it but not sure why
+        var d = Q.defer();
+        request('https://hacker-news.firebaseio.com/v0/item/'+ el +'.json', function (error, response, body) {
+            (error) ? d.reject(error) : d.resolve(JSON.parse(body));
+        });
+        return d.promise;
+    }));
+    return array;
+}
+
+function outputResults(results) {
+    var d = Q.defer();
+    results.forEach(function(value) {
+        var tile = document.createElement('div');
+        var title = document.createElement('div');
+        var company = document.createElement('div');
+        var description = document.createElement('div');
+
+        tile.className = 'jobTile';
+        title.className = 'title';
+        company.className = 'company';
+        description.className = 'description';
+
+        title.innerHTML       = value.title;
+        company.innerHTML     = value.by;
+        description.innerHTML = value.url;
+        tile.appendChild(title);
+        tile.appendChild(company);
+        tile.appendChild(description);
+        document.getElementById('container').appendChild(tile);
+        
+    });
+}
+
+requestPromise('https://hacker-news.firebaseio.com/v0/jobstories.json')
+    .then(getJobData)
+    .then(outputResults);
+
