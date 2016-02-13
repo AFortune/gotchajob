07ded556d4bdea71bcb81627242748294017d7e9
Aaron Fortune <aaron.fortune@icloud.com>
2016-02-13 12:09:54 -0500

    Moved a little around.

--- test/appSpec.js
+++ test/appSpec.js
@@ -1,11 +1,15 @@
 var app = require('../app.js');
 var appData = require('../savedJobs.json');
 var expect = require('chai').expect;
+
+
 describe('Testing of the api', function() {
+
     it('Gets job ids', function() {
         var jobIds = app.requestPromise('https://hacker-news.firebaseio.com/v0/jobstories.json');
         expect(jobIds).to.not.be.an('undefined');
     });
+
     it('Gets job info', function() {
         var jobIds = app.requestPromise('https://hacker-news.firebaseio.com/v0/jobstories.json');
         return jobIds.then(function(data) {
@@ -23,6 +27,7 @@ describe('Testing of the api', function() {
         expect(jobIds.length).to.equal(jobs.length);
     });
 });
+
 describe('Tests for saving user\'s jobs', function() {
     it('Saves the selected jobs', function() {
 
